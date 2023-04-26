afb70308ef4eb66c5c52a341aa059a17ac14dab3
Prerna Bhandari <prernabhandari00@gmail.com>
2023-04-26 23:30:24 +0530

    build

---
+++ client/build/precache-manifest.c7f4ee641de5699107c3196c0e92a0c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca389424e38c8b6b75ca1acd6b0a6750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5bcd053d57a6bed6c23",
+    "url": "/static/css/main.db9cf82f.chunk.css"
+  },
+  {
+    "revision": "9ca41ea3dd374fba26f7",
+    "url": "/static/js/2.88e24038.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.88e24038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5bcd053d57a6bed6c23",
+    "url": "/static/js/main.9b3b2cac.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "dc8c20a20360357bc4ebee39c159e782",
+    "url": "/static/media/showcase.dc8c20a2.jpg"
+  },
+  {
+    "revision": "a301654e033d71b80e09be4020ecfbd1",
+    "url": "/static/media/spinner.a301654e.gif"
+  }
+]);
